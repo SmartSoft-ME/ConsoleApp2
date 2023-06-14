09695c14c06faaeb623bb22a4eb314efa01bf26a
AliHamade <10120634@mu.edu.lb>
2023-06-14 19:26:23 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Please Enter your name: ");
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello, {0} the current time is {1}", userName, System.DateTime.Now.TimeOfDay);
+
+Console.WriteLine("Press any key to quit.");
+Console.ReadKey();
+
